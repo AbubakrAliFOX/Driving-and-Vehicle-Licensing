18273ca6c1168dd8166cccf1f614a3b32eecc39f
Abubakr <thefox5000@gmail.com>
2024-08-08 19:55:11 +0300

    Refactor person card

--- PresentationLayer/People/frmPersonDetails.Designer.cs
+++ PresentationLayer/People/frmPersonDetails.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.ctrlPersonCard1 = new PresentationLayer.ctrlPersonCard();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(596, 332);
+            this.button1.Location = new System.Drawing.Point(612, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 41);
             this.button1.TabIndex = 1;
@@ -41,14 +42,24 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // ShowPersonDetails
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(-1, 4);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.PersonInfo = null;
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(740, 384);
+            this.ctrlPersonCard1.TabIndex = 2;
+            // 
+            // frmPersonDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 400);
+            this.ClientSize = new System.Drawing.Size(740, 445);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.ctrlPersonCard1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "ShowPersonDetails";
+            this.Name = "frmPersonDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Show Details";
             this.ResumeLayout(false);
 
@@ -56,5 +67,6 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
+        private ctrlPersonCard ctrlPersonCard1;
     }
 }
--- PresentationLayer/People/frmPersonDetails.cs
+++ PresentationLayer/People/frmPersonDetails.cs
@@ -13,33 +13,11 @@ namespace PresentationLayer
 {
     public partial class frmPersonDetails : Form
     {
-        ctrlPersonCard ctrlPerson;
-        clsPerson newPerson;
-        public frmPersonDetails(int personID)
+        public frmPersonDetails(int PersonID)
         {
             InitializeComponent();
-            SetFormPosition();
-            newPerson = clsPerson.Find(personID);
-            InitializeControls();
-            AddControls();
-        }
 
-        private void SetFormPosition()
-        {
-            //this.WindowState = FormWindowState.Maximized;
-            //this.Width = Screen.PrimaryScreen.Bounds.Width;
-            this.StartPosition = FormStartPosition.CenterScreen;
-        }
-
-        private void InitializeControls()
-        {
-            ctrlPerson = new ctrlPersonCard();
-            ctrlPerson.PersonInfo = newPerson;
-        }
-
-        private void AddControls()
-        {
-            this.Controls.Add(ctrlPerson);
+            ctrlPersonCard1.PersonInfo = clsPerson.Find(PersonID);
         }
 
         private void button1_Click(object sender, EventArgs e)
